8ae55353cdcbaca5ed7ceb7637af4052001aec1f
tskon <tsekhmeister.k@gmail.com>
2022-09-26 18:34:40 +0300

    add validation logic to example

--- src/stories/form/form.tsx
+++ src/stories/form/form.tsx
@@ -1,5 +1,6 @@
-import React, {ChangeEvent, useCallback, useState} from 'react';
+import React, {ChangeEvent, FormEvent, useCallback, useState} from 'react';
 import './styles.css';
+import {useValidation} from "../../lib";
 
 export interface FormProps {
   /**
@@ -20,6 +21,15 @@ export interface FormProps {
  * Form component for user interaction
  */
 export const Form = (props: FormProps) => {
+  const v = useValidation({
+    name: [{
+      type: 'minLength',
+      value: 2
+    }],
+    email: ['required', 'email'],
+    password: ['required', 'password']
+  })
+
   const [form, setForm] = useState({
     name: props.initState?.name || '',
     email: props.initState?.email || '',
@@ -31,45 +41,66 @@ export const Form = (props: FormProps) => {
       ...state,
       [e.target.name]: e.target.value
     }))
+
+    v.validate(e.target.name, e.target.value)
   }, [])
+
+  const submitHandler = useCallback((e:FormEvent) => {
+    e.preventDefault()
+  }, [])
+
+  const inputsData = [
+    {
+      key: 'name',
+      type: 'text',
+    },
+    {
+      key: 'email',
+      type: 'email',
+    },
+    {
+      key: 'password',
+      type: 'password',
+    }
+  ]
 
   return (
     <form
       className="form"
+      onSubmit={submitHandler}
     >
-      <label>
-        <span>Name</span>
-        <input
+      {
+        inputsData.map(({type, key}) => {
+          const formKey = key as keyof typeof form
 
-            type="text"
-            name="name"
-            className="input"
-            value={form.name}
-            onInput={inputHandler}
-        />
-      </label>
-
-      <label>
-        <span>Email</span>
-        <input
-            type="email"
-            name="email"
-            className="input"
-            value={form.email}
-            onInput={inputHandler}
-        />
-      </label>
-
-      <label>
-        <span>Password</span>
-        <input
-            type="password"
-            name="password"
-            className="input"
-            value={form.password}
-            onInput={inputHandler}
-        />
-      </label>
+          return (
+              <label key={key}>
+                <span>
+                  {key.toUpperCase()}
+                </span>
+                <input
+                    type={type}
+                    name={key}
+                    className="input"
+                    value={form[key as keyof typeof form]}
+                    onInput={inputHandler}
+                />
+                <span className="v-status">
+                  Is valid:
+                  <i className={!v.isValid(formKey) ? 'negative-result' : ''}>
+                    {v.isValid(formKey) ? 'true' : 'false'}
+                  </i>
+                </span>
+                <span>
+                  Is touched:
+                  <i className={v.isUntouched(formKey) ? 'negative-result' : ''}>
+                    {v.isTouched(formKey) ? 'true' : 'false'}
+                  </i>
+                </span>
+              </label>
+          )
+        })
+      }
 
       <button className="submit-button">
         submit
