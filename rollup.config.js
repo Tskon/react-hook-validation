dd4c336301cb4ccb286fc751071f7accffd4c40e
tskon <tsekhmeister.k@gmail.com>
2022-09-27 08:42:17 +0300

    fix eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,38 @@
-import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import {terser} from 'rollup-plugin-terser'
-import dts from 'rollup-plugin-dts'
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJson = require('./package.json')
 
 export default [
-    {
-        input: 'src/lib/index.ts',
-        output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            peerDepsExternal(),
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: './tsconfig.json' }),
-            terser(),
-        ],
-    },
-    {
-        input: 'dist/esm/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-        plugins: [dts()],
-    },
+  {
+    input: 'src/lib/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({tsconfig: './tsconfig.json'}),
+      terser(),
+    ],
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{file: 'dist/index.d.ts', format: 'esm'}],
+    plugins: [dts()],
+  },
 ]
