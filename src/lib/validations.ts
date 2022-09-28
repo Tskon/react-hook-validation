2967df0ae7fa7b67731a7c833efef568322e9d27
tskon <tsekhmeister.k@gmail.com>
2022-09-28 11:41:39 +0300

    delete useless validations

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -2,8 +2,6 @@ const emailRegExp = /^[a-z0-9.]+@[a-z]+\.[a-z]{2,3}$/
 const passwordStrengthRegExp = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$/
 const telegramRegExp = /^@.{4,63}$/
 const urlRegExp = /^([(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|localhost)\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)$/
-const nameRegExp = /[\da-zA-Zа-яА-Я-]{2,}/
-const slugRegExp = /^[-0-9a-z]{2,75}$/
 const phoneRegExp = /^\+(\s?[()-]?\s?\d){5,15}$/
 
 export const required = (value: string) => {
@@ -43,18 +41,4 @@ export const url = (value: string) => {
   if (!value) return true
 
   return urlRegExp.test(value.trim())
-}
-
-// TODO delete validations below, because its for narrow usage
-
-export const name = (value: string) => {
-  if (!value) return true
-
-  return nameRegExp.test(value.trim())
-}
-
-export const slug = (value: string) => {
-  if (!value) return true
-
-  return slugRegExp.test(value.trim())
 }
