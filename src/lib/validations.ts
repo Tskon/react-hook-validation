6935a440755de131126f69d4197fd49b6239af68
tskon <tsekhmeister.k@gmail.com>
2022-10-18 09:41:33 +0300

    upd url rule

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -1,7 +1,7 @@
 const emailRegExp = /^[a-z0-9.]+@[a-z]+\.[a-z]{2,3}$/
 const passwordStrengthRegExp = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$/
 const telegramRegExp = /^@.{4,63}$/
-const urlRegExp = /^([(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|localhost)\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)$/
+const urlRegExp = /^(https?:\/\/)?([(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|localhost)\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)$/
 const phoneRegExp = /^\+(\s?[()-]?\s?\d){5,15}$/
 
 export const required = (value: string) => {
